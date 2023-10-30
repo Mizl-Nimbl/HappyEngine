90fe21eff90a47c86a43863af28c9d0fbf927eec
Mizl-Nimbl <110634762+Mizl-Nimbl@users.noreply.github.com>
2023-10-30 18:09:49 -0500

    abcdefg

---
+++ build/CMakeFiles/HappyEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HappyEngine.dir/main.cpp.o"
+  "CMakeFiles/HappyEngine.dir/main.cpp.o.d"
+  "HappyEngine"
+  "HappyEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HappyEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HappyEngine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HappyEngine.
+# This may be replaced when dependencies are built.
