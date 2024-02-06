ef9fa8e20839f4e41ed543b85e9edac47c52e37d
Mizl-Nimbl <110634762+Mizl-Nimbl@users.noreply.github.com>
2024-02-06 11:53:15 -0600

    Remove ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mizl/Documents/HappyEngine
-# Build directory: /home/mizl/Documents/HappyEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
